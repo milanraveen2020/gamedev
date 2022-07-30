5b1f2c471e68718ad242167b1d4884baca502264
Raveen <raveenn@millenniumitesp.com>
2022-07-30 17:01:30 +0530

    Remove Files

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,6 @@
 import DefaultLayout from '../layout/DefaultLayout';
 import Home from '../screens/home'
-import Create from '../screens/create';
-import ItemDetails from '../screens/itemdetails';
-import Profile from '../screens/profile';
-import Editprofile from '../screens/editprofile';
+
 
 const routes = [
     {
